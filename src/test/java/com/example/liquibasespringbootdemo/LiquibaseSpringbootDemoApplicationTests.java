48aaadce46b5a5e5e3d7f097e98f56cfa6507363
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-04-20 12:44:01 +0300

    Add Java resources

---
+++ src/test/java/com/example/liquibasespringbootdemo/LiquibaseSpringbootDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.liquibasespringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseSpringbootDemoApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
